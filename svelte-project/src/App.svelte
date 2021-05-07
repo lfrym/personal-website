21eddf0cd499fa1fc5e1dbab87566d0e3e418e8f
Luke Frymire <luke.a.frymire@gmail.com>
2021-05-07 16:17:59 -0700

    under construction message

--- svelte-project/src/App.svelte
+++ svelte-project/src/App.svelte
@@ -1,30 +1,29 @@
 <script>
-	export let name;
+	import Typewriter from './components/Typewriter.svelte'
+	let curLine = 0
+	function updateLine(event) {
+		curLine = event.detail.line;
+	}
 </script>
 
+
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<Typewriter on:doneTyping={updateLine} txt={'You\'ve found my website.'} whichLine={1}/>
+
+	{#if curLine >= 1}
+	<Typewriter on:doneTyping={updateLine} txt={'It\'s still under construction.'} whichLine={2}/>
+	{/if}
+
+	{#if curLine >= 2}
+	<Typewriter on:doneTyping={updateLine} txt={'Come back soon :)'} whichLine={3}/>
+	{/if}
+
+	
+
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
+	:global(body) {
+		background-color: rgb(26, 26, 26);
 	}
 </style>
---
+++ svelte-project/src/components/Typewriter.svelte
@@ -0,0 +1,41 @@
+<script>
+    import { createEventDispatcher } from 'svelte';
+
+    const dispatch = createEventDispatcher();
+
+
+    let i = 0;
+    let typedText = '';
+    export let txt = 'No text has been input yet...';
+    export let speed = 30;
+    export let waitTime = 750;
+    export let whichLine;
+    function typeWriter(){
+        if (i < txt.length) {
+            typedText += txt.charAt(i);
+            i++;
+            setTimeout(typeWriter, speed);
+        }
+        else {
+            setTimeout(doneTyping, waitTime);
+        }
+	}
+
+    function doneTyping(){
+        dispatch('doneTyping',{
+        line:whichLine
+        });
+    }
+    
+    typeWriter();
+    
+
+</script>
+
+<p>{typedText}</p>
+
+<style>
+    p {
+        color: white;
+    }
+</style>
